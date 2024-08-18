73f20e9664ea29cc628de569f5e088c27a9910d5
Nikhil Kumar <nknapster@gmail.com>
2024-08-18 13:36:56 +0530

    changes

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,48 @@
+import { ChevronRightIcon, HomeIcon } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+// conent
+// Founded as a pioneer in the food industry, Green Agri Corp Private Limited bridges traditional farming methods with modern technology to deliver premium rice from India's prosperous farms.
+// Our commitment to excellence:
+
+// Quality Assurance: We rigorously test samples at every stage of production to ensure the highest standards of health and quality.
+// Integrated Supply Chain: Our control over raw material sourcing, manufacturing, packaging, and distribution allows us to maintain superior quality throughout the process.
+// Customer Satisfaction: Meeting and exceeding our clients' expectations is at the core of our company strategy and goals.
+
+// At Green Agri Corp, we combine time-honored agricultural practices with innovative techniques to meet our customers' evolving needs. Our state-of-the-art infrastructure and dedication to quality position us as a leader in providing exceptional rice products to the global market.
+
+export default function AboutUsPage() {
+  return (
+    <>
+      <div className="relative bg-slate-900/40 h-full overflow-hidden ">
+
+        <div className={`bg-[url("/IMG_1.jpg")] scale-110  transition-all absolute right-0 left-0 top-0 bottom-0 -z-10 bg-cover bg-center blur-md`}>
+        </div>
+        <div className="flex items-center h-full  z-10  min-h-[100px]">
+          <div className="text-slate-200 gap-3 flex transition-all px-12 ">
+            <Link href="/">
+              <HomeIcon />
+            </Link>
+            <ChevronRightIcon />
+            About Us
+
+          </div>
+        </div>
+
+      </div>
+
+      <div className="max-w-4xl mx-auto px-5 py-20 flex flex-col gap-3  ">
+        <h1 className="text-4xl md:text-6xl font-serif ">Green Agri Corp Pvt Ltd</h1>
+        <p className="text-2xl md:text-2xl font-thin ">Founded as a pioneer in the food industry, Green Agri Corp Private Limited bridges traditional farming methods with modern technology to deliver premium rice from India's prosperous farms.</p>
+        <h2 className="text-2xl font-serif mt-5">Our commitment to excellence:</h2>
+        <ul className=" *:border  *:rounded-lg *:my-2 *:p-5 *:shadow-md  gap-2 md:gap-3 grid md:grid-cols-3 grid-cols-1 ">
+          <li>Quality Assurance: We rigorously test samples at every stage of production to ensure the highest standards of health and quality.</li>
+          <li>Integrated Supply Chain: Our control over raw material sourcing, manufacturing, packaging, and distribution allows us to maintain superior quality throughout the process.</li>
+          <li>Customer Satisfaction: Meeting and exceeding our clients' expectations is at the core of our company strategy and goals.</li>
+        </ul>
+        <p className="text-2xl md:text-2xl font-thin mt-5">At Green Agri Corp, we combine time-honored agricultural practices with innovative techniques to meet our customers' evolving needs. Our state-of-the-art infrastructure and dedication to quality position us as a leader in providing exceptional rice products to the global market.</p>
+
+      </div>
+    </>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { ArrowRight } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -17,15 +18,48 @@ export default function Home() {
               <button className="text-lg border-2 font-bold border-emerald-600 text-white px-5 py-2 rounded-full flex  items-center gap-3 bg-teal-700/30 backdrop-blur-sm">Contact Us
                 {/* <ArrowRight size={18} /> */}
               </button>
-
-              <span className="ml-5 flex  gap-3 items-center">
-                About us <ArrowRight size={18} />
-              </span>
+              <Link href="/about-us">
+                <span className="ml-5 flex  gap-3 items-center">
+                  About us <ArrowRight size={18} />
+                </span>
+              </Link>
             </div>
           </div>
         </div>
       </div>
-
+      <ShortAboutUsSection />
     </div>
   );
+}
+
+function ShortAboutUsSection() {
+  return <>
+    <div className="container mx-auto  md:px-20 px-3">
+      <div className="flex flex-col md:flex-row gap-5 py-10 [&>*]:md:max-w-[50%] items-center">
+        <div className="flex-1 grow ">
+          <h1 className="text-4xl font-serif ">Green Agri Corp Pvt Ltd</h1>
+          <p className=" text-2xl font-thin">Founded as a pioneer in the food industry, Green Agri Corp Private Limited bridges traditional farming methods with modern technology to deliver premium rice from India's prosperous farms.</p>
+          {/* find out more btn */}
+          <div className="p-2"></div>
+          <div className="flex items-center gap-3">
+            <Link href="/about-us">
+
+              <button className="
+                p-3 px-8 flex items-center justify-center gap-3 bg-amber-400 
+              ">About Us
+                <ArrowRight size={18} />
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="flex-1 md:flex justify-center items-center relative  md:p-2" >
+          {/* 2 solid shadow behind the image  like cards */}
+          <div className=" aspect-square object-cover rounded-lg shadow-lg relative max-w-sm " >
+            <div className=" absolute top-12 left-12 right-0 bottom-0 z-[-1] rounded-lg shadow-lg scale-110 bg-slate-300 transition-all "></div>
+            <Image src="/IMG_1.jpg" alt="About Us" width={500} height={500} className="object-cover h-full rounded-lg w-full" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
 }
